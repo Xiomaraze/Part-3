f64a6d0a964b4629cd07e5a7d6f50ee2790f89a4
Xiomaraze <allie.arnott@gmail.com>
2024-03-14 00:36:38 -0400

    Got the majority of the UI text part done, but i dont understand why its telling me its got a null exception for _unity_self

--- Assets/Week 9/Scripts/CharacterControl.cs
+++ Assets/Week 9/Scripts/CharacterControl.cs
@@ -1,11 +1,15 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class CharacterControl : MonoBehaviour
 {
+    public GameObject buton;
+    TextMeshProUGUI tmp;
     public static Villager SelectedVillager { get; private set; }
+
     public static void SetSelectedVillager(Villager villager)
     {
         if(SelectedVillager != null)
@@ -15,5 +19,20 @@ public class CharacterControl : MonoBehaviour
         SelectedVillager = villager;
         SelectedVillager.Selected(true);
     }
-    
+
+    private void Update()
+    {
+        tmp = buton.GetComponent <TextMeshProUGUI>();
+        string name = string.Empty;
+        if (SelectedVillager != null)
+        {
+            name = SelectedVillager.GetType().Name;
+            tmp.SetText(name);
+        }
+        else
+        {
+            tmp.SetText (string.Empty);
+        }
+    }
+
 }
